589507b292169cbac1f8b2057a91073a26e1325b
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2020-08-07 05:39:47 +0600

    first commit

---
+++ src/my_calculator/My_Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_calculator;
+
+/**
+ *
+ * @author Tahsina Keya
+ */
+public class My_Calculator {
+    
+    public static void main(String[] args) {
+        
+        new framClac().setVisible(true);
+        
+    }
+    
+}
